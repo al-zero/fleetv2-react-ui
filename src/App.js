c0a9cb16fb35101243d81ab9e1bf4537fecaebce
alpha sabawu <46676904+al-zero@users.noreply.github.com>
2022-12-29 15:39:55 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Countries from './Countries'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.js</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*  <a*/}
+      {/*    className="App-link"*/}
+      {/*    href="https://reactjs.org"*/}
+      {/*    target="_blank"*/}
+      {/*    rel="noopener noreferrer"*/}
+      {/*  >*/}
+      {/*    Learn React*/}
+      {/*  </a>*/}
+      {/*</header>*/}
+
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+            <a className="navbar-brand" href="#">Fleet MS ui react</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+                    aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                        <div className="dropdown-menu" aria-labelledby="dropdown01">
+                            <a className="dropdown-item" href="#">Action</a>
+                            <a className="dropdown-item" href="#">Another action</a>
+                            <a className="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
       <Countries></Countries>
     </div>
   );
--- src/Countries.js
+++ src/Countries.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import axios from "axios";
 
 
+
 class Countries extends Component{
 
     constructor(props) {
@@ -13,11 +14,34 @@ class Countries extends Component{
     componentDidMount(){
         axios.get('http://127.0.0.1:8082/api/countries').then(response => {
             console.log(response);
+            this.setState({countries:response.data})
         })
     }
     render() {
+        const {countries} = this.state
         return (
-            <div><h1>Country list</h1></div>
+            <div className={'container'}><h2>Country list</h2>
+                <table className="table table-striped table-advance table-hover">
+                    <thead>
+                    <tr>
+                        <td>Id</td>
+                        <td>Description</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        countries.map(function (country) {
+                           return <tr>
+                                <td>{country.id}</td>
+                                <td>{country.description}</td>
+                            </tr>
+
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
+
         )
     }
 }
